61f0dd380ae52883af6577bbc26b7b456ffd47dd
sudianlong <2907712399@qq.com>
2020-09-30 21:46:56 +0800

    完成个人中心的开发

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,91 @@
-$(function() {
-  var form = layui.form
-
+$(function () {
+  //  创建密码校验规则
+  var form = layui.form;
   form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格! '
+    ],
+    samePwd: function (value) {//  规范代码
       if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
+        return "新密码与原密码相同! "
       }
+
     },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
+    rePwd: function (value) {
+      if (value != $('[name=newPwd]').val()) {
+        return '两次密码不一致! '
       }
     }
-  })
+  });
 
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
+  //  绑定提交事件 
+  $('.layui-form').submit(function (e) {
+    e.preventDefault();
     $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
+      type: "POST",
+      url: "/my/updatepwd",
       data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+      success: function (res) {
+        if (res.status != 0) {
+          return layui.layer.msg('更新密码失败! ')
         }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
+        layui.layer.msg('更新密码成功! ');
+        // 重置表单 源生dom 表单重置
         $('.layui-form')[0].reset()
       }
     })
   })
-})
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   var form = layui.form
+
+//   form.verify({
+//     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//     samePwd: function(value) {
+//       if (value === $('[name=oldPwd]').val()) {
+//         return '新旧密码不能相同！'
+//       }
+//     },
+//     rePwd: function(value) {
+//       if (value !== $('[name=newPwd]').val()) {
+//         return '两次密码不一致！'
+//       }
+//     }
+//   })
+
+//   $('.layui-form').on('submit', function(e) {
+//     e.preventDefault()
+//     $.ajax({
+//       method: 'POST',
+//       url: '/my/updatepwd',
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layui.layer.msg('更新密码失败！')
+//         }
+//         layui.layer.msg('更新密码成功！')
+//         // 重置表单
+//         $('.layui-form')[0].reset()
+//       }
+//     })
+//   })
+// })
